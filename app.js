7517fa71717c368c69c77f5a88dffd4b61c3dd93
tstillwell2k17 <tristanstillwell@gmail.com>
2017-09-02 21:37:16 -0400

    model skeleton

---
+++ app.js
@@ -0,0 +1,11 @@
+// check local storage first for points, if they dont exist read this
+
+var model = {
+	places : [
+		{ name: "first place",
+		  latlng: { 100, 100 } ,
+		  wiki_link: "...", // to get wiki article for place
+		},{...},{...},{...}
+	]
+};	
+// then save to localstorage  
