f54a30a8616ab6b36a7e3333558de9bf756764e1
tstillwell2k17 <tristanstillwell@gmail.com>
2017-09-02 22:41:49 -0400

    Added some places to model

--- app.js
+++ app.js
@@ -2,10 +2,22 @@
 
 var model = {
 	places : [
-		{ name: "first place",
-		  latlng: { 100, 100 } ,
-		  wiki_link: "...", // to get wiki article for place
-		},{...},{...},{...}
+		{ name: "Vulcan Statue",
+		  latlng: { 33.4917, -86.795537} ,
+		  wiki_link: "https://en.wikipedia.org/wiki/Vulcan_statue"
+		},
+		{ name: "Sloss Furnaces",
+		  latlng: { 33.520651, -86.791061  } ,
+		  wiki_link: "https://en.wikipedia.org/wiki/Sloss_Furnaces"
+		},
+		{ name: "McWane Science Center",
+		  latlng: { 33.514785, -86.808295} ,
+		  wiki_link: "https://en.wikipedia.org/wiki/McWane_Science_Center"
+		},
+		{ name: "Railroad Park",
+		  latlng: { 33.508301, -86.811972 } ,
+		  wiki_link: "https://en.wikipedia.org/wiki/Railroad_Park"
+		},
 	]
-};	
+};
 // then save to localstorage  
