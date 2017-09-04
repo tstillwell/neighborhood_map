10af85687b546db860b157b2d7a67b19fe893d45
tstillwell2k17 <tristanstillwell@gmail.com>
2017-09-03 23:04:33 -0400

    Implemented places data saved to localStorage

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-// check local storage first for points, if they dont exist read this
-
-var model = {
-	selected_place: null,
-	places: [
-		{ name: "Vulcan Statue",
-		  position: { lat: 33.4917, lng: -86.795537},
-		  wiki_link: "https://en.wikipedia.org/wiki/Vulcan_statue"
-		},
-		{ name: "Sloss Furnaces",
-		  position: { lat: 33.520651, lng: -86.791061  } ,
-		  wiki_link: "https://en.wikipedia.org/wiki/Sloss_Furnaces"
-		},
-		{ name: "McWane Science Center",
-		  position: { lat: 33.514785, lng: -86.808295} ,
-		  wiki_link: "https://en.wikipedia.org/wiki/McWane_Science_Center"
-		},
-		{ name: "Railroad Park",
-		  position: { lat: 33.508301, lng: -86.811972 } ,
-		  wiki_link: "https://en.wikipedia.org/wiki/Railroad_Park"
-		},
-		{ name: "Birmingham Zoo",
-		  position: { lat: 33.486009, lng: -86.779541 } ,
-		  wiki_link: "https://en.wikipedia.org/wiki/Birmingham_Zoo"
-		},
-		{ name: "Birmingham Civil Rights Institute",
-		  position: { lat: 33.516092, lng: -86.814521 } ,
-		  wiki_link: "https://en.wikipedia.org/wiki/Birmingham_Civil_Rights_Institute"
-		}
-	]
-};
-// then save to localstorage  
+if (localStorage.map_places){
+	all_points = localStorage.map_places;
+	}
+// check local storage first for points, if they dont exist read this	
+else {
+	var model = {
+		selected_place: null,
+		places: [
+			{ name: "Vulcan Statue",
+			  position: { lat: 33.4917, lng: -86.795537},
+			  wiki_link: "https://en.wikipedia.org/wiki/Vulcan_statue"
+			},
+			{ name: "Sloss Furnaces",
+			  position: { lat: 33.520651, lng: -86.791061  } ,
+			  wiki_link: "https://en.wikipedia.org/wiki/Sloss_Furnaces"
+			},
+			{ name: "McWane Science Center",
+			  position: { lat: 33.514785, lng: -86.808295} ,
+			  wiki_link: "https://en.wikipedia.org/wiki/McWane_Science_Center"
+			},
+			{ name: "Railroad Park",
+			  position: { lat: 33.508301, lng: -86.811972 } ,
+			  wiki_link: "https://en.wikipedia.org/wiki/Railroad_Park"
+			},
+			{ name: "Birmingham Zoo",
+			  position: { lat: 33.486009, lng: -86.779541 } ,
+			  wiki_link: "https://en.wikipedia.org/wiki/Birmingham_Zoo"
+			},
+			{ name: "Birmingham Civil Rights Institute",
+			  position: { lat: 33.516092, lng: -86.814521 } ,
+			  wiki_link: "https://en.wikipedia.org/wiki/Birmingham_Civil_Rights_Institute"
+			}
+		]
+	};
+	localStorage.map_places = model; // save to local storage for future use
+	all_points = model;
+}
