b12cfc9c203c4fa344ea9198220dd459d4ad5af7
tstillwell2k17 <tristanstillwell@gmail.com>
2017-09-07 22:37:29 -0400

    Added click bindings for place list

--- app.js
+++ app.js
@@ -42,9 +42,12 @@ else {
 var view_model = {
 		place_list : ko.observableArray(all_points.places), // to create item list
 		
+		selectPlace : function (place) { // called when clicking item in list
+			console.log(place);
+		},
 		// read data from models to create markers
 		marker_array : all_points.places,
-		initMap : function () { // make global so gmapsapi can use for callback
+		initMap : function () {
 			console.log("adding map to page");
 			var gmap = new google.maps.Map(document.getElementById('map'), {
 				center: {lat: 33.517641, lng: -86.802979},
